f75d3ef7a944213a4dc086e774cf921937f4fc1d
Heather Nuffer <zzzuzz@gmail.com>
2020-01-17 18:35:41 -0700

    Added GET route to server

--- server.js
+++ server.js
@@ -6,4 +6,19 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.status(200).json({message: "Welcome to the accounts API"});
+});
+
+server.get('/api/accounts', (req, res) => {
+    db('accounts')
+        .then(response => {
+            console.log(response);
+            res.json(response);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+});
+
 module.exports = server;
