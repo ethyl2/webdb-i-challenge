964642fdd3a067edffbf34ca17090dfc32c06285
Heather Nuffer <zzzuzz@gmail.com>
2020-01-17 22:39:01 -0700

    Added middleware to validate id for accounts

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -20,7 +20,7 @@ router.get('/:id', (req, res) => {
     db('accounts').where({id: id}).first()
         .then(response => {
             console.log(response);
-            if (response) {
+            if (response !== undefined) {
                 res.status(200).json(response);
             } else {
                 res.status(500).json({message: `There was an error while retrieving the account with id ${id}`})
@@ -54,5 +54,49 @@ router.post('/', (req, res) => {
             res.status(500).json({error: err, message: "Unable to insert new account."});
         });
 });
+
+router.put('/:id', validateId, (req, res) => {
+    const payload = {
+        name: req.body.name,
+        budget: req.body.budget
+    };
+    db('accounts').where({id: req.params.id}).update(payload)
+        .then(response => {
+            console.log(response);
+            //res.status(201).json(response);
+            if (response !== 1) {
+                res.status(500).json({message: `Unable to update account of id ${req.params.id}`});
+            } else {
+                db('accounts').where({id :req.params.id}).first()
+                .then(newResponse => {
+                    res.status(201).json(newResponse);
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json({error: err, message: `There was an error while retrieving the new account, which has the id of ${req.params.id}`});
+                });  
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err, message: `Unable to update account of id ${req.params.id}`});
+        });
+});
+
+function validateId(req, res, next) {
+    const id = req.params.id;
+    db('accounts').where({id: id}).first()
+        .then(response => {
+            if (response !== undefined) {
+            next();
+            } else {
+                res.status(500).json({message: `Unable to retrieve account of id ${id}`});
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err, message: `Unable to retrieve account of id ${id}`});
+        });
+};   
 
 module.exports = router;
