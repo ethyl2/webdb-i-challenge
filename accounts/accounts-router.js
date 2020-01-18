47f5326e7a3645e780396ae03dcfd008852df8a1
Heather Nuffer <zzzuzz@gmail.com>
2020-01-17 18:53:35 -0700

    Added GET to account by id

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -15,4 +15,21 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    db('accounts').where({id: id}).first()
+        .then(response => {
+            console.log(response);
+            if (response) {
+                res.status(200).json(response);
+            } else {
+                res.status(500).json({message: `There was an error while retrieving the account with id ${id}`})
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err, message: `There was an error while retrieving the account with id ${id}`});
+        });
+});
+
 module.exports = router;
